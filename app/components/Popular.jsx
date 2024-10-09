c34a8a88549d36441ffdad61dd18ec250e065572
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-09 21:18:25 +0300

    feat: nav bar state controlled

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,19 +1,39 @@
 import * as React from "react";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+  }
+
+  updateLanguage = (selectedLanguage) => {
+    this.setState({
+      selectedLanguage,
+    });
+  };
+
   render() {
     const languages = ["All", "JavaSript", "Ruby", "Java", "CSS", "Python"];
 
     return (
-      <select>
-        {languages.map((language) => {
-          return (
-            <option key={language} value={language}>
-              {language}
-            </option>
-          );
-        })}
-      </select>
+      <main>
+        <select
+          onChange={(e) => this.updateLanguage(e.target.value)}
+          value={this.state.selectedLanguage}
+        >
+          {languages.map((language) => {
+            return (
+              <option key={language} value={language}>
+                {language}
+              </option>
+            );
+          })}
+        </select>
+        {JSON.stringify(this.state, null, 2)}
+      </main>
     );
   }
 }
